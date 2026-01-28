c950192e786410ad0cdd7d822a96010ab28924fb
aman singh <amansinghpatel429@gmail.com>
2026-01-28 20:45:54 +0530

    Backend Services
    All Backend Microservices structure

---
+++ project-service/src/test/java/project_service/project_service/ProjectServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package project_service.project_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
