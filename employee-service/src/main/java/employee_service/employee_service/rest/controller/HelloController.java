78723e27c39a2e37e0236d2beaa95f7f66f08ce5
aman singh <amansinghpatel429@gmail.com>
2026-01-29 23:57:08 +0530

    good to start application on swagger and 8081 port
    good to start application on swagger and 8081 port

---
+++ employee-service/src/main/java/employee_service/employee_service/rest/controller/HelloController.java
@@ -0,0 +1,12 @@
+package employee_service.employee_service.rest.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String hello() {
+        return "Hello World!";
+    }
+}
