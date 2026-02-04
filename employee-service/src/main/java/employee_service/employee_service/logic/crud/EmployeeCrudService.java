b9026b1630bd4e1469c0f48a335f6e3bc46a5af5
aman singh <amansinghpatel429@gmail.com>
2026-02-04 23:10:49 +0530

    EmployeeCrudService class
    EmployeeCrudService class

---
+++ employee-service/src/main/java/employee_service/employee_service/logic/crud/EmployeeCrudService.java
@@ -0,0 +1,4 @@
+package employee_service.employee_service.logic.crud;
+
+public interface EmployeeCrudService {
+}
---
+++ employee-service/src/main/java/employee_service/employee_service/logic/crud/EmployeeCrudServiceImpl.java
@@ -0,0 +1,4 @@
+package employee_service.employee_service.logic.crud;
+
+public class EmployeeCrudServiceImpl implements EmployeeCrudService {
+}
